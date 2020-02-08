f6d09d3d59670fbaa0ffb5030d46d9d3ea4df715
April John Maraat <aprilmaraat@gmail.com>
2020-02-08 20:35:47 +0800

    GenericService methods.

--- src/app/services/generic.service.ts
+++ src/app/services/generic.service.ts
@@ -2,7 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 export class GenericService {
+    public base: string;
     public endpoint: string;
+    public http: HttpClient;
 
-    constructor() {}
+    constructor(_http: HttpClient) {
+        this.http = _http;
+    }
+
+    public get(id: number): Observable<any> {
+        let result: Observable<any>;
+        return this.http.get<Observable<any>>(this.endpoint);
+    }
+
+    public post(object: any): Observable<any> {
+        let result: Observable<any>;
+        return this.http.post<Observable<any>>(this.endpoint, object);
+    }
 }
