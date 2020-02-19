73c52ea624ed0b8e3844a45e892cb31cc8f9cd74
April John Maraat <aprilmaraat@gmail.com>
2020-02-20 01:02:22 +0800

    Initial load overlay code.

---
+++ src/app/custom-modules/load-overlay/load-overlay.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'q-load-overlay',
+    templateUrl: './load-overlay.component.html',
+    styleUrls: ['./load-overlay.component.scss']
+})
+export class LoadOverlayComponent {
+    
+}
